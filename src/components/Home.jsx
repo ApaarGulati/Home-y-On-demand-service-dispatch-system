6dc0db49d944f479fdd43b914c7375cb332619cc
Herumb <herumb2312@gmail.com>
2026-04-18 13:24:15 +0530

    added conditional rendering to user home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,26 @@ const Home = () => {
     var wordarr = ["Search for massages", "Search for Servicing", "Search for Facials"];
     const navigate=useNavigate();
     const [query,setquery]=useState("");
+    const [isUser,setisUser]=useState(true);
+
 
     const handlesearch=(input) => {
       localStorage.setItem("Searchquery",input);
       navigate("/services")
 
     }
+
+
+    useEffect(() => {
+      var output=localStorage.getItem("userRole");
+      if(output==="app_user"){
+        setisUser(true);
+      }else{
+        setisUser(false);
+      }
+    
+    }, [])
+    
     
 
 
@@ -26,34 +40,34 @@ const Home = () => {
             <Usernav />
             <ScrollFadeIn>
                 <div className=' w-full h-100   flex flex-row flex-wrap justify-center  items-center  gap-4 bg-transparent'>
-                    <p className='text-center text-7xl font-extrabold dark:text-white'>Home services at </p> <p className='text-center text-cyan-500 text-6xl font-extrabold'>your doorstep</p>
+                    {isUser==true?<><p className='text-center text-7xl font-extrabold dark:text-white'>Home services at </p> <p className='text-center text-cyan-500 text-6xl font-extrabold'>your doorstep</p></>: <><p className='text-center text-7xl font-extrabold dark:text-white'>Own your craft.</p> <p className='text-center text-cyan-500 text-6xl font-extrabold'>Own your time.</p></> }
                 </div>
 
-                <div className='flex flex-row flex-wrap  gap-4 justify-center'>
+                {isUser&&<div className='flex flex-row flex-wrap  gap-4 justify-center'>
                     <AnimatedInput type="text" value={query} onChange={(e)=>setquery(e.target.value)} className="w-100 h-15 pl-2 text-[20px] rounded-xl text  border border-gray-400 bg-white bg-no-repeat bg-left bg-contain focus:outline-2 focus: outline-cyan-500 focus:outline-offset-4 focus:border focus:border-cyan-500 "   placeholders={wordarr}   />
                     <button className="bg-cyan-500 w-15 h-15 text-center text-white rounded-full  flex flex-row items-center justify-center  font-bold text-[12px] cursor-pointer hover:bg-cyan-700" onClick={()=>handlesearch(query)}>
                         <img src={search} className='object-contain h-10 w-10'  alt="" />
                     </button>
-
-
-                </div>
+                </div>}
             </ScrollFadeIn>
 
-            <ScrollFadeIn>
+
+            {isUser&&<><ScrollFadeIn>
                 <Carousel text="Recommended for you"/>
             </ScrollFadeIn>
-
 
             <ScrollFadeIn>
                 <Carousel text="Quickies"/>
             </ScrollFadeIn>
 
-
             <ScrollFadeIn>
                 <Carousel text="Deep shit"/>
-            </ScrollFadeIn>
+            </ScrollFadeIn></>}
 
             <Footer/>
+
+
+            
 
 
 
