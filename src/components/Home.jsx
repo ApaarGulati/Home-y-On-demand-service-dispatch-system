949f1fa1874f2646e869819632d86c5cad0a8f76
Daksh <krdaksh05@gmail.com>
2026-04-18 13:28:14 +0530

    wor2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Usernav from './Usernav';
 import ScrollFadeIn from './ScrollFadeIn';
 import { AnimatedInput } from './AnimatedInput';
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,6 +14,7 @@ import worker from "../assets/workerworking.png"
 import { useNavigate } from 'react-router-dom'
 
 
+
 const Landing = () => {
 
     var wordarr = ["Search for massages", "Search for Servicing", "Search for Facials"];
